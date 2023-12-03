eda7d0727773972bef7a22fbd1bae353c4bec9fc
ariframaps <ariframa@student.ub.ac.id>
2023-12-03 18:51:51 +0700

    mengambil value dari register form, dan berhasil menyambungkan projek ke firebase

--- app/src/main/java/com/andhikaihsansapplication/app/modules/register/ui/RegisterActivity.kt
+++ app/src/main/java/com/andhikaihsansapplication/app/modules/register/ui/RegisterActivity.kt
@@ -3,6 +3,8 @@ package com.andhikaihsansapplication.app.modules.register.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.andhikaihsansapplication.app.R
 import com.andhikaihsansapplication.app.appcomponents.base.BaseActivity
@@ -21,6 +23,34 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(R.layout.activity
   }
 
   override fun setUpClicks(): Unit {
+    binding.txtRegisterOne.setOnClickListener{
+      // ambil value username
+      val usernameInput = findViewById<EditText>(R.id.lineLineOne)
+      val usernameInputValue = usernameInput.text.toString()
+
+      // ambil value email
+      val emailInput = findViewById<EditText>((R.id.lineLineTwo))
+      val emailInputValue = emailInput.text.toString()
+
+      // ambil value full name
+      val fullnameInput = findViewById<EditText>((R.id.lineLineThree))
+      val fullnameInputVaue = fullnameInput.text.toString()
+
+      // ambil value phone number
+      val phonenumberInput = findViewById<EditText>((R.id.lineLineFour))
+      val phonenumberInputValue = phonenumberInput.text.toString()
+
+      // ambil value password
+      val passwordInput = findViewById<EditText>((R.id.lineLineFive))
+      val passwordInputVaue = passwordInput.text.toString()
+
+      // ambil value confirm password
+      val confirmPassword = findViewById<EditText>((R.id.lineLineSix))
+      val confirmpasswordInputValue = confirmPassword.text.toString()
+
+      Toast.makeText(this, "username = $usernameInputValue", Toast.LENGTH_SHORT).show()
+    }
+
     binding.txtLogin.setOnClickListener {
       val destIntent = LoginActivity.getIntent(this, null)
       startActivity(destIntent)
