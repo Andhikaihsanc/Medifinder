9cc35587cba83aa18a94ca6ff88d96da0bfcbffa
ariframaps <ariframa@student.ub.ac.id>
2023-12-05 03:51:11 +0700

    berhasil membuat list sesuai dummy

---
+++ app/src/main/java/com/andhikaihsansapplication/app/modules/spesialisjantung/recyclerHandler/RumahSakitAdapter.kt
@@ -0,0 +1,40 @@
+package com.andhikaihsansapplication.app.modules.spesialisjantung.recyclerHandler
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.andhikaihsansapplication.app.R
+
+class RumahSakitAdapter(
+    private val rumahSakitList: List<RumahSakit>,
+    private val clickListener: RumahSakitClickListener
+) : RecyclerView.Adapter<RumahSakitAdapter.RumahSakitViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RumahSakitViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.row_listbrawijayaunive, parent, false)
+        return RumahSakitViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: RumahSakitViewHolder, position: Int) {
+        val currentItem = rumahSakitList[position]
+        holder.textNamaRumahSakit.text = currentItem.nama
+        holder.textAlamatRumahSakit.text = currentItem.alamat
+        holder.textTeleponRumahSakit.text = currentItem.telepon
+
+        holder.itemView.setOnClickListener {
+            clickListener.onRumahSakitClicked(currentItem)
+        }
+    }
+
+    override fun getItemCount() = rumahSakitList.size
+
+    class RumahSakitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textNamaRumahSakit: TextView = itemView.findViewById(R.id.txtBrawijayaUnive)
+        val textAlamatRumahSakit: TextView = itemView.findViewById(R.id.txtTimeZone)
+        val textTeleponRumahSakit: TextView = itemView.findViewById(R.id.txt0341403000)
+
+    }
+}
--- app/src/main/java/com/andhikaihsansapplication/app/modules/spesialisjantung/ui/SpesialisJantungActivity.kt
+++ app/src/main/java/com/andhikaihsansapplication/app/modules/spesialisjantung/ui/SpesialisJantungActivity.kt
@@ -7,11 +7,16 @@ import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.andhikaihsansapplication.app.R
 import com.andhikaihsansapplication.app.appcomponents.base.BaseActivity
 import com.andhikaihsansapplication.app.databinding.ActivitySpesialisJantungBinding
 import com.andhikaihsansapplication.app.modules.spesialisjantung.`data`.model.ListbrawijayauniveRowModel
 import com.andhikaihsansapplication.app.modules.spesialisjantung.`data`.viewmodel.SpesialisJantungVM
+import com.andhikaihsansapplication.app.modules.spesialisjantung.recyclerHandler.RumahSakit
+import com.andhikaihsansapplication.app.modules.spesialisjantung.recyclerHandler.RumahSakitAdapter
+import com.andhikaihsansapplication.app.modules.spesialisjantung.recyclerHandler.RumahSakitClickListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -22,14 +27,34 @@ import kotlin.String
 import kotlin.Unit
 
 class SpesialisJantungActivity :
-    BaseActivity<ActivitySpesialisJantungBinding>(R.layout.activity_spesialis_jantung) {
+    BaseActivity<ActivitySpesialisJantungBinding>(R.layout.activity_spesialis_jantung), RumahSakitClickListener {
   private val viewModel: SpesialisJantungVM by viewModels<SpesialisJantungVM>()
+
+  private lateinit var recyclerView: RecyclerView
+  private lateinit var rumahSakitAdapter: RumahSakitAdapter
 
   override fun onResume() {
     super.onResume()
 
     val listHeaderText = findViewById<TextView>(R.id.listHeaderText)
     listHeaderText.text = intent.getStringExtra("CURRENT_KATEGORI")
+
+    val rumahSakitList = listOf(
+      RumahSakit("Rumah Sakit A", "Jl. A No. 1", "0813356188823"),
+      RumahSakit("Rumah Sakit B", "Jl. B No. 2", "0813356188823"),
+      RumahSakit("Rumah Sakit C", "Jl. C No. 3", "0813356188823")
+    )
+
+    recyclerView = findViewById(R.id.recyclerListbrawijayaunive)
+    recyclerView.layoutManager = LinearLayoutManager(this)
+    rumahSakitAdapter = RumahSakitAdapter(rumahSakitList, this)
+    recyclerView.adapter = rumahSakitAdapter
+  }
+
+  override fun onRumahSakitClicked(rumahSakit: RumahSakit) {
+    // Handle klik pada item rumah sakit di sini
+    // Contoh: Tampilkan pesan Toast
+    Toast.makeText(this, "Anda memilih ${rumahSakit.nama}", Toast.LENGTH_SHORT).show()
   }
 
   override fun onInitialized(): Unit {
