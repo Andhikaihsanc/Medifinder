5e7f5469a648a13726d1a2a82ddc609b42dd5587
ariframaps <ariframa@student.ub.ac.id>
2023-12-04 22:18:05 +0700

    berhasil menambahkan fitur update user data

--- app/src/main/java/com/andhikaihsansapplication/app/modules/changeaddress/ui/ChangeAddressActivity.kt
+++ app/src/main/java/com/andhikaihsansapplication/app/modules/changeaddress/ui/ChangeAddressActivity.kt
@@ -3,11 +3,19 @@ package com.andhikaihsansapplication.app.modules.changeaddress.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.andhikaihsansapplication.app.R
 import com.andhikaihsansapplication.app.appcomponents.base.BaseActivity
 import com.andhikaihsansapplication.app.databinding.ActivityChangeAddressBinding
 import com.andhikaihsansapplication.app.modules.changeaddress.`data`.viewmodel.ChangeAddressVM
+import com.andhikaihsansapplication.app.modules.editprofile.ui.EditProfileActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlin.String
 import kotlin.Unit
 
@@ -21,6 +29,34 @@ class ChangeAddressActivity :
   }
 
   override fun setUpClicks(): Unit {
+    // firebase auth and current user
+    var auth = FirebaseAuth.getInstance();
+    var currentUserEmail = auth.currentUser?.email
+
+    // firebase realtime
+    var database = FirebaseDatabase.getInstance()
+    var usersRef = database.getReference("users")
+    var query = usersRef.orderByChild("email").equalTo(currentUserEmail.toString())
+
+    binding.btnChangeAddress.setOnClickListener{
+      val address = findViewById<EditText>(R.id.lineLineEleven)
+      val addressValue = address.text.toString()
+
+      // mencari username
+      query.addListenerForSingleValueEvent(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+          for (userSnapshot in snapshot.children) {
+            Toast.makeText(baseContext, userSnapshot.key.toString(), Toast.LENGTH_SHORT).show()
+            usersRef.child(userSnapshot.key.toString()).child("address").setValue(addressValue)
+          }
+        }
+
+        override fun onCancelled(error: DatabaseError) {}
+      })
+
+      val destIntent = EditProfileActivity.getIntent(this, null)
+      startActivity(destIntent)
+    }
   }
 
   companion object {
--- app/src/main/java/com/andhikaihsansapplication/app/modules/changename/ui/ChangeNameActivity.kt
+++ app/src/main/java/com/andhikaihsansapplication/app/modules/changename/ui/ChangeNameActivity.kt
@@ -3,11 +3,20 @@ package com.andhikaihsansapplication.app.modules.changename.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.andhikaihsansapplication.app.R
 import com.andhikaihsansapplication.app.appcomponents.base.BaseActivity
 import com.andhikaihsansapplication.app.databinding.ActivityChangeNameBinding
 import com.andhikaihsansapplication.app.modules.changename.`data`.viewmodel.ChangeNameVM
+import com.andhikaihsansapplication.app.modules.deleteaccconfirmation.ui.DeleteAccConfirmationActivity
+import com.andhikaihsansapplication.app.modules.editprofile.ui.EditProfileActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlin.String
 import kotlin.Unit
 
@@ -20,6 +29,34 @@ class ChangeNameActivity : BaseActivity<ActivityChangeNameBinding>(R.layout.acti
   }
 
   override fun setUpClicks(): Unit {
+    // firebase auth and current user
+    var auth = FirebaseAuth.getInstance();
+    var currentUserEmail = auth.currentUser?.email
+
+    // firebase realtime
+    var database = FirebaseDatabase.getInstance()
+    var usersRef = database.getReference("users")
+    var query = usersRef.orderByChild("email").equalTo(currentUserEmail.toString())
+
+    binding.btnChangeName.setOnClickListener{
+      val newFullName = findViewById<EditText>(R.id.lineLineEleven)
+      val newFullNameValue = newFullName.text.toString()
+
+      // mencari username
+      query.addListenerForSingleValueEvent(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+          for (userSnapshot in snapshot.children) {
+            Toast.makeText(baseContext, userSnapshot.key.toString(), Toast.LENGTH_SHORT).show()
+            usersRef.child(userSnapshot.key.toString()).child("fullName").setValue(newFullNameValue)
+          }
+        }
+
+        override fun onCancelled(error: DatabaseError) {}
+      })
+
+      val destIntent = EditProfileActivity.getIntent(this, null)
+      startActivity(destIntent)
+    }
   }
 
   companion object {
