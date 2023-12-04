5797ccafeca1900f071a9dbd9a8dd232d9b23c5e
ariframaps <ariframa@student.ub.ac.id>
2023-12-05 03:51:11 +0700

    berhasil membuat list sesuai dummy

---
+++ app/src/main/java/com/andhikaihsansapplication/app/modules/spesialisjantung/recyclerHandler/RumahSakit.kt
@@ -0,0 +1,3 @@
+package com.andhikaihsansapplication.app.modules.spesialisjantung.recyclerHandler
+
+data class RumahSakit(val nama: String, val alamat: String, val telepon: String)
---
+++ app/src/main/java/com/andhikaihsansapplication/app/modules/spesialisjantung/recyclerHandler/RumahSakitClickListener.kt
@@ -0,0 +1,5 @@
+package com.andhikaihsansapplication.app.modules.spesialisjantung.recyclerHandler
+
+interface RumahSakitClickListener {
+    fun onRumahSakitClicked(rumahSakit: RumahSakit)
+}
